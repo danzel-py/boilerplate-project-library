0c12ad34c3de5e77069f8fd72a0e392b3b7b5af8
danzelpy <danzel@students.itb.ac.id>
2021-03-29 11:23:37 +0000

    api.js is perfect

--- routes/api.js
+++ routes/api.js
@@ -14,15 +14,16 @@ module.exports = function(app, collection) {
 
   app.route('/api/books')
     .get(function(req, res) {
+      //response will be array of book objects
+      //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
       collection.find().toArray((err, col) => {
         if (err) return console.log(err)
         res.send(col)
       })
-      //response will be array of book objects
-      //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
     })
 
     .post(function(req, res) {
+      //response will contain new book object including atleast _id and title
       let title = req.body.title;
       if (!title) return res.send('missing required field title')
       collection.insertOne({
@@ -33,40 +34,32 @@ module.exports = function(app, collection) {
         if (err) return console.log("can't insert book")
         res.json(book.ops[0])
       })
-      //response will contain new book object including atleast _id and title
     })
 
     .delete(function(req, res) {
+      //if successful response will be 'complete delete successful'
       collection.deleteMany({}, (err) => {
         if (err) return console.log(err)
         res.send('complete delete successful')
       })
-      //if successful response will be 'complete delete successful'
     });
-
-
 
   app.route('/api/books/:id')
     .get(function(req, res) {
-      let bookid = req.params.id;
       //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
+      let bookid = req.params.id;
       collection.findOne({ _id: ObjectID(bookid) }, (err, book) => {
         if (err) return console.log(err)
         if (!book) return res.send('no book exists')
         res.json(book)
       })
     })
-//HERER
 
-
-
-
-// SSSS
     .post(function(req, res) {
+      //json res format same as .get
       let bookid = req.params.id;
       let comment = req.body.comment;
       if (!comment) return res.send('missing required field comment')
-      //json res format same as .get
       collection.findOneAndUpdate({ _id: ObjectID(bookid) },
         {
           $push: { comments: comment },
@@ -75,15 +68,17 @@ module.exports = function(app, collection) {
           returnOriginal: false
         } ,(err, book) => {
           if (err) return console.log(err)
+          // Book not found
           if (!book.value) return res.send('no book exists')
+          // Book found
           res.json(book.value)
           
         })
     })
 
     .delete(function(req, res) {
-      let bookid = req.params.id;
       //if successful response will be 'delete successful'
+      let bookid = req.params.id;
       collection.findOne({ _id: ObjectID(bookid) }, (err, book) => {
         if (err) return console.log(err)
         if (!book) return res.send('no book exists')
